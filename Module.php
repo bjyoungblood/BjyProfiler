a3bab5f25db6a1958a48c7be839afb0393502da6
Manuel St. <manuakasam@googlemail.com>
2012-09-26 10:42:58 +0300

    Update Module.php
    Without the getConfig() the configuration for this module (mainly the alteration of Zend\Db\Adapter\Adapter) won't be loaded and the profiling can't be used out of the box from ZendDeveloperTools

--- Module.php
+++ Module.php
@@ -4,6 +4,11 @@ namespace BjyProfiler;
 
 class Module
 {
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
     public function getAutoloaderConfig()
     {
         return array(
