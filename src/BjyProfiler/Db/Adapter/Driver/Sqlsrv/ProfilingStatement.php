a4b3bb826559b662f1c9c11424991eaf6b06d6a1
Ben Youngblood <bx.youngblood@gmail.com>
2012-11-27 03:47:07 -0700

    turns out that DEBUG_BACKTRACE_IGNORE_ARGS is necessary, and passing false isn't good enough
    Revert "remove dep on php 5.3.6"
    
    This reverts commit 08edcfb2e37cc8a179f1b16001b0857a5fe94b0a.

--- src/BjyProfiler/Db/Adapter/Driver/Sqlsrv/ProfilingStatement.php
+++ src/BjyProfiler/Db/Adapter/Driver/Sqlsrv/ProfilingStatement.php
@@ -21,7 +21,7 @@ class ProfilingStatement extends Statement
             $saveParams = $parameters;
         }
 
-        $stack = debug_backtrace(false);
+        $stack = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
 
         $queryId = $this->getProfiler()->startQuery($this->getSql(), $saveParams, $stack);
         $result = parent::execute($parameters);
