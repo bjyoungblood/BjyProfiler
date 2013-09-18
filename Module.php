3ef331c66f15c527aac17470d6710e4722956903
Dmitriy Detyuk <ddetyuk@gmail.com>
2013-09-18 19:22:38 +0300

    added autoload_classmap.php and moved closure from config file to getServiceConfig in module.php

--- Module.php
+++ Module.php
@@ -4,6 +4,7 @@ namespace BjyProfiler;
 
 class Module
 {
+
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
@@ -12,6 +13,9 @@ class Module
     public function getAutoloaderConfig()
     {
         return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
@@ -19,4 +23,32 @@ class Module
             ),
         );
     }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Zend\Db\Adapter\Adapter' => function ($sm) {
+                    $config = $sm->get('Configuration');
+                    if (!isset($config['db'])) {
+                        return false;
+                    }
+                    if (class_exists('BjyProfiler\Db\Adapter\ProfilingAdapter')) {
+                        $adapter = new \BjyProfiler\Db\Adapter\ProfilingAdapter($config['db']);
+                        $adapter->setProfiler(new \BjyProfiler\Db\Profiler\Profiler);
+                        if (isset($config['db']['options']) && is_array($config['db']['options'])) {
+                            $options = $config['db']['options'];
+                        } else {
+                            $options = array();
+                        }
+                        $adapter->injectProfilingStatementPrototype($options);
+                    } else {
+                        $adapter = new \Zend\Db\Adapter\Adapter($config['db']);
+                    }
+                    return $adapter;
+                },
+            ),
+        );
+    }
+
 }
