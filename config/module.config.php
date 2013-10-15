6ab5fbaee5e361a6fda0677037c5727ccaf22aa2
Mickaël Zhu <m.zhu@epokmedia.fr>
2013-10-15 15:50:02 +0200

    Use ProfilingAdapterFactory instead of closure in module config

--- config/module.config.php
+++ config/module.config.php
@@ -2,25 +2,7 @@
 return array(
     'service_manager' => array(
         'factories' => array(
-            'Zend\Db\Adapter\Adapter' => function ($sm) {
-                $config = $sm->get('Configuration');
-                if(!isset($config['db'])){
-                    return false;
-                }
-                if(class_exists('BjyProfiler\Db\Adapter\ProfilingAdapter')){
-                    $adapter = new BjyProfiler\Db\Adapter\ProfilingAdapter($config['db']);
-                    $adapter->setProfiler(new BjyProfiler\Db\Profiler\Profiler);
-                    if (isset($config['db']['options']) && is_array($config['db']['options'])) {
-                        $options = $config['db']['options'];
-                    } else {
-                        $options = array();
-                    }
-                    $adapter->injectProfilingStatementPrototype($options);
-                } else {
-                    $adapter = new Zend\Db\Adapter\Adapter($config['db']);
-                }
-                return $adapter;
-            },
+            'Zend\Db\Adapter\Adapter' => 'BjyProfiler\Db\Adapter\ProfilingAdapterFactory'
         ),
     ),
 );
