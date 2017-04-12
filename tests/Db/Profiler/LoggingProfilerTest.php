f83608055332d0fed8be88a990d432b511418bf8
Andrey N. Mokhov <a.mokhov@magdv.com>
2017-04-12 23:30:44 +0700

    added integration tests, fix phpunit default configuration

---
+++ tests/Db/Profiler/LoggingProfilerTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andi
+ * Date: 12.04.17
+ * Time: 0:18
+ */
+
+namespace BjyProfilerTest\Db\Profiler;
+
+
+use BjyProfiler\Db\Profiler\LoggingProfiler;
+use PHPUnit\Framework\TestCase;
+
+class LoggingProfilerTest extends TestCase
+{
+
+}
---
+++ tests/IntegrationTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andi
+ * Date: 12.04.17
+ * Time: 21:41
+ */
+
+namespace BjyProfilerTest;
+
+
+use ${TESTED_NAME};
+use PHPUnit\Framework\TestCase;
+
+class IntegrationTest extends TestCase
+{
+
+}
